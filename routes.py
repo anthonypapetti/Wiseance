8e2b1a22f3a4281f425074b9812a927480dd6b0b
anthonypapetti <apapetti@outlook.com>
2020-07-10 18:40:24 -0400

    Added a help page for amazon link errors

--- routes.py
+++ routes.py
@@ -24,6 +24,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         session["user_id"] = user.id
+        return redirect('/')
     return render_template("login.html", form=form)
 
 @app.route("/logout")
@@ -115,3 +116,8 @@ def deleteamazon():
     db.session.delete(data)
     db.session.commit()
     return jsonify(True)
+
+@app.route("/amznhelp")
+@login_required
+def amznhelp():
+    return render_template("amznhelp.html")
